e4f2e01f63ceb600c7b48af8d1330fa16bf59f99
anthony mcmullen <jondouglasheit@gmail.com>
2020-04-29 12:02:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,10 +7,10 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  SELECT projects.title, pledges.amount 
+  SELECT Projects.title, Pledges.amount 
   FROM projects
   INNER JOIN pledges
-  ON projects.id = pledges.project_id 
+  ON Projects.id = Pledges.project_id 
   ORDER by projects.title;
   
   puts "projects.title , pledges.amount"
