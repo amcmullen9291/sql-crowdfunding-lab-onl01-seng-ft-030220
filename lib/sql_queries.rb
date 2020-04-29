334fbecca57cef548678be72a4a1869a0db4d248
anthony mcmullen <jondouglasheit@gmail.com>
2020-04-29 11:56:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 # Write your sql queries in this file in the appropriate method like the example below:
 #
 # def select_category_from_projects
-# "SELECT carojectstegory FROM projects;"
+# "SELECT category FROM projects;"
 # end
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   FROM projects
   INNER JOIN pledges
   ON projects.id = project.id 
-  ORDER by titles;
+  ORDER by projects.titles;
   
 end
 
